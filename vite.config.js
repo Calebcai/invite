0db6c82e6a3be0969c66aa76a052b17b084f58f5
calevb <876057433@qq.com>
2024-08-29 20:18:45 +0800

    提交

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/Calebcai/invite',
+  base:'https://github.com/Calebcai/invite/dist/',
   server: {
     port: 3000,
     host: true,
